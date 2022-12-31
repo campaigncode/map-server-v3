c2cacb285257921cd2e9f30de48d72996ef40fa4
RahulR100 <rahulrao@email.com>
2022-12-31 11:00:09 -0800

    add pip

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ apt update
 apt install -y libicu66
 npm i tileserver-gl
 echo "Downloading files..."
-pip install gdown
+pip3 install gdown
 gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/view?usp=share_link
 echo "Ready!"
 tileserver-gl
