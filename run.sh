3ec4684a7ca13d53865cb0b12fa5679900b5bbda
RahulR100 <rahulrao@email.com>
2022-12-31 10:40:41 -0800

    config change

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo "Starting..."
 echo "Configuring..."
 echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee -a /etc/apt/sources.list
 sudo apt update
-sudo apt install -y libicu66
-npm i -g tileserver-gl
+sudo apt install -y python3 libicu66 libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++ xvfb libgles2-mesa-dev libgbm-dev libxxf86vm-dev
+npm i tileserver-gl
 echo "Downloading files..."
 pip install gdown
 gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/view?usp=share_link
