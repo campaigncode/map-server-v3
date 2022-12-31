04aa1a92b95993241f9fa112e341057f67e5fe09
RahulR100 <rahulrao@email.com>
2022-12-31 10:45:22 -0800

    remove sudo

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 echo "Starting..."
 echo "Configuring..."
-echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt install -y python3 libicu66 libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++ xvfb libgles2-mesa-dev libgbm-dev libxxf86vm-dev
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | tee -a /etc/apt/sources.list
+apt update
+apt install -y libicu66
 npm i tileserver-gl
 echo "Downloading files..."
 pip install gdown
