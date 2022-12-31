9cfd8c9fcc42b131ee5e22c0d1856f5a10dce135
RahulR100 <rahulrao@email.com>
2022-12-31 10:32:55 -0800

    change build order

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "Starting..."
+echo "Configuring..."
 echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee -a /etc/apt/sources.list
 sudo apt update
-sudo apt install libicu66
+sudo apt install -y libicu66
+npm i -g tileserver-gl
+echo "Downloading files..."
+pip install gdown
+gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/view?usp=share_link
+echo "Ready!"
 tileserver-gl
