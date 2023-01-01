750a04eb37cc855537b4a006a0d859207c19a96b
RahulR100 <rahulrao@email.com>
2023-01-01 12:32:09 -0800

    update bashrc

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 echo "deb http://security.ubuntu.com/ubuntu focal-security main" | tee -a /etc/apt/sources.list
 apt update
 apt install -y libicu66
+
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/' >>~/.bashrc
+source ~/.bashrc
