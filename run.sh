576be94cecba53622489dfa48d5882ac1342be7d
RahulR100 <rahulrao@email.com>
2023-01-01 12:38:24 -0800

    move ld

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/
 
 echo "Ready!"
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/
+
 tileserver-gl
