2db62228e1f0a135a9312878f4a7afae2b0a4f58
RahulR100 <rahulrao@email.com>
2023-01-01 12:08:37 -0800

    add curl

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Downloading server files..."
-
-npm i tileserver-gl@4.3.3
-
 echo "Downloading tiles..."
 
 gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/view?usp=share_link
