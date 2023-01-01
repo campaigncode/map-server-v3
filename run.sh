e2a7d540f30923db191a66955fbc72b4fe595040
RahulR100 <rahulrao@email.com>
2023-01-01 10:27:07 -0800

    try adding to nixpath

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 echo "Starting..."
 echo "Configuring..."
-export PATH=/usr/local/bin:$PATH
+
 echo "deb http://security.ubuntu.com/ubuntu focal-security main" | tee -a /etc/apt/sources.list
 apt update
-apt install -y libicu66 libuuid1
-npm i tileserver-gl
+apt install -y libicu66
+
 echo "Downloading files..."
+
 pip3 install gdown
 gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/view?usp=share_link
+
 echo "Ready!"
+
 tileserver-gl
