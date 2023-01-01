e6adb72bc5745c230f8335a3d03d417ad289762d
RahulR100 <rahulrao@email.com>
2023-01-01 10:38:32 -0800

    add libgl

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo "Starting..."
-echo "Configuring..."
-
-echo "deb http://security.ubuntu.com/ubuntu focal-security main" | tee -a /etc/apt/sources.list
-apt update
-apt install -y libicu66
-
 echo "Downloading files..."
 
-pip3 install gdown
 gdown --fuzzy https://drive.google.com/file/d/1aqbfEDwqM17DmRM6hNRAdO7tM3GrmZVF/view?usp=share_link
 
 echo "Ready!"
